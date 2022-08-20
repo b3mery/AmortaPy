d931d26e314900a63c90b16b774bd21678a53e26
Ben <ben3670@live.com.au>
2022-08-20 17:31:11 +1000

    Remove 'loan' from function name

--- AmortaPy/__init__.py
+++ AmortaPy/__init__.py
@@ -5,7 +5,7 @@ from ._api import *
 
 __all__ = [
     'generate_amortization_table',
-    'generate_loan_amortization_schedule',
+    'generate_amortization_schedule',
     'Amortization',
     'calculate_total_period_payment',
     'calculate_principal_and_interest_payment',
--- AmortaPy/_api.py
+++ AmortaPy/_api.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from .core._amortization import Amortization
 
 
-def generate_loan_amortization_schedule(nominal_annual_interest_rate:float,
+def generate_amortization_schedule(nominal_annual_interest_rate:float,
                                         loan_amount:int|float,
                                         years:int|float,
                                         repayment_frequency:str|int|float|None = None) -> Amortization:
