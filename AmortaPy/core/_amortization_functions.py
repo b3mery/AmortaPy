580ebf14d98aabda9979d8ead406d8246fa31d75
Ben <ben3670@live.com.au>
2022-08-27 11:16:56 +1000

    Interest only options for amort table

--- AmortaPy/core/_amortization_functions.py
+++ AmortaPy/core/_amortization_functions.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from argparse import ArgumentError
 
 import pandas as pd
 
@@ -71,22 +72,30 @@ def generate_amortization_table(nominal_interest_rate_per_period:float,
                                 principal_amount:int|float,
                                 number_of_periods:int|float,
                                 total_payment_per_period: int|float|None=None,
-                                additional_payment_per_period: int|float|None=None ) -> pd.DataFrame:
+                                additional_payment_per_period: int|float|None=None,
+                                interest_only_rate_per_period:float=0.00,
+                                number_of_interest_only_periods:int|float=0 ) -> pd.DataFrame:
     """Generate a Amortization Repaymet table as a `pandas.DataFrame`
 
     Args:
-        * nominal_interest_rate_per_period (float): The quote annual interest rate `3.85%` devided by the repayment frequency. Monthly would be passed as `0.0385/12`
+        * nominal_interest_rate_per_period (float): The quote annual interest rate `3.85%` divided by the repayment frequency. Monthly would be passed as `0.0385/12`
         * principal_amount (int | float): The principal amount borrowed `545000.00`
         * number_of_periods (int | float): Number of periods over the life of the loan. EG: `30 years` paid `monthly` = `30*12` 
         * total_payment_per_period (int | float | None, optional): Optional total payment per period `PMT` you intend to make. Defaults to None. Func will calculate the payment by default.
         If `total_payment_per_period` is passed it will need to be >= the expected minium `PMT`, if it is not, the calculated minium PMT will be used. 
         * additional_payment_per_period (int | float | None, optional): Optional additional payment per period you intend to make, will be added to `total_payment_per_period`. Defaults to None.
-    
+        * interest_only_rate_per_period (int | float, optional): The quote annual interest rate for the intertest periods, `4.14%` divided by the repayment frequency. Monthly would be passed as `0.0414/12`
+        * number_of_interest_only_periods (int | float, optional): The interest only periods of the loan. EG: `1 year` paid `monthly` = `1*12` 
+
     Returns:
          pd.DataFrame: Amortization Repayment Schedule Table
     """
     # Evaluate the Params
-    expected_total_period_payment = calculate_total_period_payment(principal_amount, nominal_interest_rate_per_period, number_of_periods)
+    if (interest_only_rate_per_period > 0.00 and number_of_interest_only_periods <= 0) or (interest_only_rate_per_period <= 0.00 and number_of_interest_only_periods > 0): 
+        raise ArgumentError(None, "To calculate interest only, you need to pass valid args to `interest_only_rate_per_period` and `number_of_interest_only_periods`")
+    
+    n_principal_payment_periods = number_of_periods - number_of_interest_only_periods
+    expected_total_period_payment = calculate_total_period_payment(principal_amount, nominal_interest_rate_per_period, n_principal_payment_periods)
 
     if total_payment_per_period is None or total_payment_per_period < expected_total_period_payment:
         total_payment_per_period = expected_total_period_payment
@@ -107,18 +116,27 @@ def generate_amortization_table(nominal_interest_rate_per_period:float,
     # Iterate through periods until the peirods are complete or the loan is $0.00
     opening_loan_balance = principal_amount
     for period in range(1, number_of_periods+1):
-        principal, interest = calculate_principal_and_interest_payment(total_payment_per_period, opening_loan_balance, nominal_interest_rate_per_period)
+        
+        if (interest_only_rate_per_period > 0.00 and number_of_interest_only_periods > 0) and period <= number_of_interest_only_periods:
+            interest = opening_loan_balance * interest_only_rate_per_period
+            principal = 0 
+            period_payment = interest
+
+        else:
+            principal, interest = calculate_principal_and_interest_payment(total_payment_per_period, opening_loan_balance, nominal_interest_rate_per_period)
+            period_payment = total_payment_per_period
+
         closing_loan_balance = opening_loan_balance - principal
         if closing_loan_balance < 0:
             principal += closing_loan_balance
             closing_loan_balance = 0
-            total_payment_per_period = principal + interest
+            period_payment = principal + interest
 
         data['period'].append(period)
         data['opening_balance'].append(opening_loan_balance)
         data['interest'].append(interest)
         data['principal'].append(principal)
-        data['period_payment'].append(total_payment_per_period)
+        data['period_payment'].append(period_payment)
         data['closing_balance'].append(round(closing_loan_balance, 6))
 
         opening_loan_balance = closing_loan_balance
